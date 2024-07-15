a16bfcefabb4e3cab2a0b16a6c4d544dad44e02c
朱重迁 <z32664206862022@163.com>
2024-07-16 00:10:02 +0800

    first commit

---
+++ new-yolov8/ultralytics-main/index.py
@@ -0,0 +1,8 @@
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO(r"F:\down\yolov8\yolov8_pt\yolov8n-pose.pt")  # load a pretrained model (recommended for training)
+
+# Train the model
+# results = model.train(data="coco-pose.yaml", epochs=100, imgsz=640)
+model.predict(source=r'F:\down\yolov8\new-yolov8\ultralytics-main\pedestrian.mp4',save=True,show=True)
